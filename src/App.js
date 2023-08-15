de85520267dfb157784002b44363a517cbf04043
monacodev <nassim.chabane@gmail.com>
2023-08-15 23:40:33 +0200

    mon premier exo react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+import Header from './Header';
+
+
 
 function App() {
+
+  let [compteur, setCompteur] = useState(0);
+
+  let [mode, setMode] = useState("normal");
+
+  useEffect(() => {
+    console.log(`le guerrier est en mode ${mode}`)
+  }, [])
+
+
+  // State j'ai définit un compteur que j'ai initialisé à un et que je peux modifier que par la fonction SetCompteur
+  //state( état,fonction pour modifier des données )
+
+  const Header = function Header(props) {
+
+
+    // les states données 
+
+    // comportement
+
+    // affichage 
+
+    return (
+
+      <h1>{props.titre}</h1>
+
+    );
+  }
+
+  //comportement
+  const increment = () => {
+    console.log("j'ai cliqué");
+    //compteur = compteur + 1;
+    //console.log(compteur);
+    setCompteur(compteur + 1);
+  }
+
+
+  // affichage
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header titre="gestion des events " />
+
+      <Header titre="deuxième titre pour le fun qui ne respecte la règle html un seul h1 " />
+
+      <p> mode  {mode}  </p>
+      <p> <button onClick={() => setMode("sayan")}>Sayan</button> <button onClick={() => setMode("Mega guerrier")}>super guerrier</button></p>
+      <p>compteur : {compteur}</p>
+
+      <button onClick={increment}> incrémenter </button>
+
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+
+function Header() {
+    // les states données 
+
+    // comportement
+
+    // affichage 
+
+    return (
+
+        <h1>titre 1</h1>
+
+    );
+}
+
+export default Header;
